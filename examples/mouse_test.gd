cad6cbeda07f953a663cf0151d25612726247098
skooter500 <skooter500@gmail.com>
2026-02-16 11:43:20 +0000

    x

--- examples/mouse_test.gd
+++ examples/mouse_test.gd
@@ -5,4 +5,4 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		print(event.position.x)
 		print(event.position.y)
-		$sinwavegenerator.set_frequency(event.position.x * 2)
+		
