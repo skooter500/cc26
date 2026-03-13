b97f582948c73cd64b0e979b4cf4be9fbfbbd8fc
skooter500 <skooter500@gmail.com>
2026-03-13 12:56:15 +0000

    grid notes

--- week_7a.gd
+++ week_7a.gd
@@ -4,16 +4,36 @@ var size = 12
 
 @export var font:Font
 
+func change_instrument(channel, instrument):
+	var midi_event = InputEventMIDI.new()
+	midi_event.channel = 0
+	midi_event.message = MIDI_MESSAGE_PROGRAM_CHANGE
+	midi_event.instrument = instrument
+	$MidiPlayer.receive_raw_midi_message(midi_event)
+	
+
+var last_note = -1
 func _input(event: InputEvent) -> void:
 	var vs = get_viewport_rect().size
-	
+
+	if event is InputEventMIDI:
+		play_note(event.pitch, 1, 0)
+		
 	if event is InputEventMouseMotion:
 		var r:int = remap(event.position.y, 0, vs.y, 0, size)
 		var c:int = remap(event.position.x, 0, vs.x, 0, size)
 	
 		var note = (r * size) + c
-		print(note)
-		play_note(note, 0.2, 0)
+		if note != last_note:
+			if major:
+				play_note(note, 2, 0)
+				play_note(note + 3, 2, 0)
+				play_note(note + 5, 2, 0)
+			else:
+				play_note(note, 2, 0)
+				play_note(note + 4, 2, 0)
+				play_note(note + 6, 2, 0)
+			last_note = note
 
 func _ready() -> void:
 	
@@ -22,12 +42,16 @@ func _ready() -> void:
 	var sum = 0
 	for i in range(17):
 		sum += x
-	
+	change_instrument(0, 25)
 	print(sum)
+	OS.open_midi_inputs()
 	
 	pass
 	
 func play_note(note, duration, channel):
+	
+	note = note % 127
+	
 	var m = InputEventMIDI.new()
 	m.message = MIDI_MESSAGE_NOTE_ON
 	m.pitch = note
@@ -61,3 +85,9 @@ func _draw() -> void:
 			draw_string(ThemeDB.fallback_font, Vector2(x + box_size * 0.1, y + box_size * 0.7), str(r) + "," + str(c), HORIZONTAL_ALIGNMENT_CENTER
 			,0,18, Color.WHITE)
 	pass
+
+var major = false
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	major = toggled_on
+	pass # Replace with function body.
